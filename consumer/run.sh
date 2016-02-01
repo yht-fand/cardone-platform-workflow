90908cf145bb355014b148bda94ee1b8e464bbb3
yao hai tao <addday@gmail.com>
2016-02-01 20:49:17 +0800

    init

--- consumer/run.sh
+++ consumer/run.sh
@@ -5,5 +5,5 @@ if [ -e 'jdk' ]; then
 export JAVA_HOME=$myself/jdk
 export PATH=$JAVA_HOME/bin:$PATH
 fi
-java -Dfile.encoding=UTF-8 -cp $myself/*;$myself/target/* org.springframework.boot.loader.JarLauncher --app.root=$myself
+java -Dfile.encoding=UTF-8 -cp $myself/*:$myself/target/* org.springframework.boot.loader.JarLauncher --app.root=$myself
 sh run.sh
