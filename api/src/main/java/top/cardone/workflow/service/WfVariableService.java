ad817fa0d1121fc688ca0bace552e7426d0405f3
addday <addday@gmail.com>
2018-04-09 11:41:34 +0800

    配置优化：减少 ${app.root}变量的使用，改进为使用file:

--- api/src/main/java/top/cardone/workflow/service/WfVariableService.java
+++ api/src/main/java/top/cardone/workflow/service/WfVariableService.java
@@ -1,9 +1,7 @@
 package top.cardone.workflow.service;
 
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 import top.cardone.cache.Caches;
 import top.cardone.context.annotation.Event;
@@ -12,7 +10,6 @@ import top.cardone.context.event.SimpleErrorEvent;
 import top.cardone.context.event.SimpleEvent;
 import top.cardone.data.service.PageService;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,194 +22,6 @@ import java.util.Map;
 @Events({@Event(applicationEvent = SimpleErrorEvent.class),
         @Event(applicationEvent = SimpleEvent.class)})
 public interface WfVariableService extends PageService {
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#page
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Page<Map<String, Object>> pageCache(Object page) {
-        return this.page(page);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#page
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> Page<P> pageCache(Class<P> mappedClass, Object page) {
-        return this.page(mappedClass, page);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#findList
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> List<P> findListCache(Class<P> mappedClass, Object findList) {
-        return this.findList(mappedClass, findList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#findOne
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> P findOneCache(Class<P> mappedClass, Object findOne) {
-        return this.findOne(mappedClass, findOne);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#readList
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <R> List<R> readListCache(Class<R> requiredType, Object readList) {
-        return this.readList(requiredType, readList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#readOne
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <R> R readOneCache(Class<R> requiredType, Object readOne) {
-        return this.readOne(requiredType, readOne);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#delete
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteCache(Object delete) {
-        return this.delete(delete);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#deleteAll
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteAllCache() {
-        return this.deleteAll();
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#deleteByIds
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteByIdsCache(Object ids) {
-        return this.deleteByIds(ids);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#deleteList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] deleteListCache(List<Object> deleteList) {
-        return this.deleteList(deleteList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#findList
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Map<String, Object>> findListCache(Object findList) {
-        return this.findList(findList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#findOne
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Map<String, Object> findOneCache(Object findOne) {
-        return this.findOne(findOne);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#insert
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int insertCache(Object insert) {
-        return this.insert(insert);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#insertByNotExists
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int insertByNotExistsCache(Object insert) {
-        return this.insertByNotExists(insert);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#insertList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] insertListCache(List<Object> insertList) {
-        return this.insertList(insertList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#insertListByNotExists
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] insertListByNotExistsCache(List<Object> insertList) {
-        return this.insertListByNotExists(insertList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#readList
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Object> readListCache(Object readList) {
-        return this.readList(readList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#readOne
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Object readOneCache(Object readOne) {
-        return this.readOne(readOne);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#save
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int saveCache(Object save) {
-        return this.save(save);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#update
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int updateCache(Object update) {
-        return this.update(update);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#updateList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] updateListCache(List<Object> updateList) {
-        return this.updateList(updateList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableService#saveList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] saveListCache(List<Object> saveList) {
-        return this.saveList(saveList);
-    }
-	
     /**
      * 查询工作流变量对象
      *
@@ -220,25 +29,15 @@ public interface WfVariableService extends PageService {
      * @return 工作流变量对象
      */
     Map<String, Object> findOneByWfVariableId(Map<String, Object> findOne);
-	
+
     /**
      * 查询工作流变量对象
      *
      * @param findOne 工作流变量标识
      * @return 工作流变量对象
      */
+    @Cacheable(key = Caches.KEY_1)
     default Map<String, Object> findOneByWfVariableIdCache(Map<String, Object> findOne) {
         return this.findOneByWfVariableId(findOne);
-    }
-	
-    /**
-     * 查询工作流变量下拉列表
-     *
-     * @param findList 关键字
-     * @return 工作流变量下拉列表
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Map<String, Object>> findListByKeywordCache(Map<String, Object> findList) {
-        return this.findListByKeyword(findList);
     }
 }
--- api/src/main/java/top/cardone/workflow/service/WfVariableUserService.java
+++ api/src/main/java/top/cardone/workflow/service/WfVariableUserService.java
@@ -1,9 +1,7 @@
 package top.cardone.workflow.service;
 
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 import top.cardone.cache.Caches;
 import top.cardone.context.annotation.Event;
@@ -12,7 +10,6 @@ import top.cardone.context.event.SimpleErrorEvent;
 import top.cardone.context.event.SimpleEvent;
 import top.cardone.data.service.PageService;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,194 +22,6 @@ import java.util.Map;
 @Events({@Event(applicationEvent = SimpleErrorEvent.class),
         @Event(applicationEvent = SimpleEvent.class)})
 public interface WfVariableUserService extends PageService {
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#page
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Page<Map<String, Object>> pageCache(Object page) {
-        return this.page(page);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#page
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> Page<P> pageCache(Class<P> mappedClass, Object page) {
-        return this.page(mappedClass, page);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#findList
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> List<P> findListCache(Class<P> mappedClass, Object findList) {
-        return this.findList(mappedClass, findList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#findOne
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> P findOneCache(Class<P> mappedClass, Object findOne) {
-        return this.findOne(mappedClass, findOne);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#readList
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <R> List<R> readListCache(Class<R> requiredType, Object readList) {
-        return this.readList(requiredType, readList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#readOne
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <R> R readOneCache(Class<R> requiredType, Object readOne) {
-        return this.readOne(requiredType, readOne);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#delete
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteCache(Object delete) {
-        return this.delete(delete);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#deleteAll
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteAllCache() {
-        return this.deleteAll();
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#deleteByIds
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteByIdsCache(Object ids) {
-        return this.deleteByIds(ids);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#deleteList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] deleteListCache(List<Object> deleteList) {
-        return this.deleteList(deleteList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#findList
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Map<String, Object>> findListCache(Object findList) {
-        return this.findList(findList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#findOne
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Map<String, Object> findOneCache(Object findOne) {
-        return this.findOne(findOne);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#insert
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int insertCache(Object insert) {
-        return this.insert(insert);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#insertByNotExists
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int insertByNotExistsCache(Object insert) {
-        return this.insertByNotExists(insert);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#insertList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] insertListCache(List<Object> insertList) {
-        return this.insertList(insertList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#insertListByNotExists
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] insertListByNotExistsCache(List<Object> insertList) {
-        return this.insertListByNotExists(insertList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#readList
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Object> readListCache(Object readList) {
-        return this.readList(readList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#readOne
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Object readOneCache(Object readOne) {
-        return this.readOne(readOne);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#save
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int saveCache(Object save) {
-        return this.save(save);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#update
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int updateCache(Object update) {
-        return this.update(update);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#updateList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] updateListCache(List<Object> updateList) {
-        return this.updateList(updateList);
-    }
-
-	/**
-     * @see top.cardone.workflow.service.WfVariableUserService#saveList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] saveListCache(List<Object> saveList) {
-        return this.saveList(saveList);
-    }
-	
     /**
      * 查询工作流变量与用户对象
      *
@@ -220,25 +29,15 @@ public interface WfVariableUserService extends PageService {
      * @return 工作流变量与用户对象
      */
     Map<String, Object> findOneByWfVariableUserId(Map<String, Object> findOne);
-	
+
     /**
      * 查询工作流变量与用户对象
      *
      * @param findOne 工作流变量与用户标识
      * @return 工作流变量与用户对象
      */
+    @Cacheable(key = Caches.KEY_1)
     default Map<String, Object> findOneByWfVariableUserIdCache(Map<String, Object> findOne) {
         return this.findOneByWfVariableUserId(findOne);
-    }
-	
-    /**
-     * 查询工作流变量与用户下拉列表
-     *
-     * @param findList 关键字
-     * @return 工作流变量与用户下拉列表
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Map<String, Object>> findListByKeywordCache(Map<String, Object> findList) {
-        return this.findListByKeyword(findList);
     }
 }
