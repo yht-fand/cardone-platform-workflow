c6b1ffbd4553f47e1e7dbdc7f7ae67879e152d45
addday <addday@gmail.com>
2018-04-11 13:09:58 +0800

    代码优化

--- implements/default/src/main/java/top/cardone/workflow/dao/WfVariableUserDao.java
+++ implements/default/src/main/java/top/cardone/workflow/dao/WfVariableUserDao.java
@@ -2,8 +2,6 @@ package top.cardone.workflow.dao;
 
 import top.cardone.data.dao.PageDao;
 
-import java.util.Map;
-
 /**
  * 工作流变量与用户
  *
--- implements/default/src/main/java/top/cardone/workflow/dao/impl/WfVariableDaoImpl.java
+++ implements/default/src/main/java/top/cardone/workflow/dao/impl/WfVariableDaoImpl.java
@@ -2,8 +2,6 @@ package top.cardone.workflow.dao.impl;
 
 import top.cardone.data.jdbc.dao.impl.PageDaoImpl;
 
-import java.util.Map;
-
 /**
  * 工作流变量
  *
